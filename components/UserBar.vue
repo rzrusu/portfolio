bd1cbac6c31b19acb87fd131535b382a2fa0edaa
rzrusu <rzrusu03@gmail.com>
2025-03-02 11:08:28 +0200

    feat: Enhance UserBar with interactive social links and font styling

--- components/UserBar.vue
+++ components/UserBar.vue
@@ -4,24 +4,25 @@
       <img src="~/assets/avatar.png" alt="logo" class="w-24 h-24 rounded-full">
     </div>
     <div class="text-white text-center space-y-2">
-        <h1 class="text-2xl font-bold">Rusu Răzvan</h1>
-        <p class="text-sm text-[#CCCCCC]">Web Dev & Designer</p>
+        <h1 class="text-2xl font-bold font-instrument">Rusu Răzvan</h1>
+        <p class="text-sm text-[#CCCCCC] font-geist">Web Dev & Designer</p>
     </div>
 
     <div class="grid grid-cols-4 border-t border-white/20">
-        <div class="w-16 h-16 border-r border-white/20 flex items-center justify-center">
+        <a href="https://github.com/rzrusu" class="w-full h-16 border-r border-white/20 hover:bg-white/10 transition-all duration-300 flex items-center justify-center">
             <img src="~/assets/icons/github.svg" alt="icon" class="w-8 h-8">
-        </div>
-        <div class="w-16 h-16 border-r border-white/20 flex items-center justify-center">
+        </a>
+        <a href="#" class="w-full h-16 border-r border-white/20 hover:bg-white/10 transition-all duration-300 flex items-center justify-center">
             <img src="~/assets/icons/linkedin.svg" alt="icon" class="w-8 h-8">
-        </div>
-        <div class="w-16 h-16 border-r border-white/20 flex items-center justify-center">
+        </a>
+        <a href="mailto:rzrusu03@gmail.com" class="w-full h-16 border-r border-white/20 hover:bg-white/10 transition-all duration-300 flex items-center justify-center">
             <img src="~/assets/icons/email.svg" alt="icon" class="w-8 h-8">
-        </div>
-        <div class="w-16 h-16 flex items-center justify-center">
+        </a>
+        <a href="#" class="w-full h-16 hover:bg-white/10 transition-all duration-300 flex items-center justify-center">
             <img src="~/assets/icons/discord.svg" alt="icon" class="w-8 h-8">
-        </div>
+        </a>
     </div>
   </div>
 </template>
+
 
