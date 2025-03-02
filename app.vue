f00a13ab6410815ac4d143a187d5d10c706ef221
rzrusu <rzrusu03@gmail.com>
2025-03-02 12:32:44 +0200

    feat: Add Project component to showcase current projects

--- app.vue
+++ app.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="max-w-3xl mx-auto py-24">
-    <div class="grid grid-cols-6 gap-4">
+    <div class="grid grid-cols-6 gap-12">
       <div class="col-span-2 text-white">
         <UserBar />
       </div>
-      <div class="col-span-4 text-white">test</div>
+      <div class="col-span-4 text-white">
+        <div class="space-y-4">
+          <h1 class="text-2xl font-bold font-instrument">currently working on</h1>
+          <div class="border border-white/20 rounded-lg">
+            <Project title="updateharbor.com" img="/logos/uh-logo.png" description="Open-source static release notes" :isLast="false" :links="[{ icon: 'uil:github', url: 'https://github.com/updateharbor/updateharbor' }]" />
+            <Project title="swiftlogs.io" img="/logos/sl-logo.png" description="Generate release notes from GitHub commit history" :isLast="false" :links="[{ icon: 'uil:link', url: 'https://swiftlogs.io' }]" />
+            <Project title="myspecs.io" img="/logos/ms-logo.png" description="Your computer's social media profile" :isLast="true" :links="[{ icon: 'uil:link', url: 'https://myspecs.io' }]" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
---
+++ components/Project.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex flex-row space-x-4 pl-4 pt-4 h-fit">
+    <img :src="img" alt="project image" class="w-12 h-12 object-cover rounded-lg">
+    <div class="w-full h-fit pb-4" :class="{'border-b border-white/20': !isLast}">
+        <div class="flex flex-row items-center space-x-2">
+            <h1 class="text-lg font-bold font-instrument">{{ title }}</h1>
+            <a v-for="link in links" :href="link.url" class="text-sm text-gray-300 font-geist">
+                <Icon :name="link.icon" style="width: 16px; height: 16px;" />
+            </a>
+        </div>
+        <p class="text-sm text-gray-300 font-geist">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+const props = defineProps({
+    title: String,
+    img: String,
+    description: String,
+    isLast: Boolean,
+    links: {
+        type: Array,
+        default: () => []
+    }
+})
+</script>
+
+
+
+
