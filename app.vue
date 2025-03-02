c25f7d76aa99388e7cf4a5ea3fcd2369bd4c8c40
rzrusu <rzrusu03@gmail.com>
2025-03-02 10:35:24 +0200

    feat: Implement basic app layout with UserBar component

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="max-w-3xl mx-auto py-16">
+    <div class="grid grid-cols-6 gap-4">
+      <div class="col-span-2 text-white">
+        <UserBar />
+      </div>
+      <div class="col-span-4 text-white">test</div>
+    </div>
   </div>
 </template>
+
+
+<style>
+  body {
+    background-color: #07090E;
+    background-image: url('~/assets/backgrounds/bg.png');
+    background-repeat: no-repeat;
+    background-position: top center;
+    background-attachment: fixed;
+    background-size: contain;
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ components/UserBar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-red-500">
+    <h1>UserBar</h1>
+  </div>
+</template>
+
