19327829c78234220bca6c2f852fb17a5e4c10ef
rzrusu <rzrusu03@gmail.com>
2025-03-02 10:50:42 +0200

    feat: Style and enhance UserBar component with personal details and social links

--- components/UserBar.vue
+++ components/UserBar.vue
@@ -1,6 +1,27 @@
 <template>
-  <div class="bg-red-500">
-    <h1>UserBar</h1>
+  <div class="border border-white/20 rounded-lg relative pt-20 space-y-12">
+    <div class="absolute -top-12 left-1/2 transform -translate-x-1/2">
+      <img src="~/assets/avatar.png" alt="logo" class="w-24 h-24 rounded-full">
+    </div>
+    <div class="text-white text-center space-y-2">
+        <h1 class="text-2xl font-bold">Rusu Răzvan</h1>
+        <p class="text-sm text-[#CCCCCC]">Web Dev & Designer</p>
+    </div>
+
+    <div class="grid grid-cols-4 border-t border-white/20">
+        <div class="w-16 h-16 border-r border-white/20 flex items-center justify-center">
+            <img src="~/assets/icons/github.svg" alt="icon" class="w-8 h-8">
+        </div>
+        <div class="w-16 h-16 border-r border-white/20 flex items-center justify-center">
+            <img src="~/assets/icons/linkedin.svg" alt="icon" class="w-8 h-8">
+        </div>
+        <div class="w-16 h-16 border-r border-white/20 flex items-center justify-center">
+            <img src="~/assets/icons/email.svg" alt="icon" class="w-8 h-8">
+        </div>
+        <div class="w-16 h-16 flex items-center justify-center">
+            <img src="~/assets/icons/discord.svg" alt="icon" class="w-8 h-8">
+        </div>
+    </div>
   </div>
 </template>
 
