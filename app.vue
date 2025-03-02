6e05230627dbd0a310785f8922bd1b3938d00c66
rzrusu <rzrusu03@gmail.com>
2025-03-02 23:14:02 +0200

    feat: Add FreelanceProject section and enhance portfolio layout

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
       <div class="col-span-2 text-white">
         <UserBar />
       </div>
-      <div class="col-span-4 text-white">
+      <div class="col-span-4 text-white space-y-8">
         <div class="space-y-4">
           <h1 class="text-2xl font-bold font-instrument">currently working on</h1>
           <div class="border border-white/20 rounded-lg">
@@ -12,6 +12,30 @@
             <Project title="swiftlogs.io" img="/logos/sl-logo.png" description="Generate release notes from GitHub commit history" :isLast="false" :links="[{ icon: 'uil:link', url: 'https://swiftlogs.io' }]" />
             <Project title="myspecs.io" img="/logos/ms-logo.png" description="Your computer's social media profile" :isLast="true" :links="[{ icon: 'uil:link', url: 'https://myspecs.io' }]" />
           </div>
+        </div>
+        <div class="space-y-4">
+          <h1 class="text-2xl font-bold font-instrument">featured freelance work</h1>
+          <div class="grid grid-cols-2 gap-4">
+            <FreelanceProject title="Sparked Host" img="/sparked.png" description="Website Design" link="https://sparkedhost.com" />
+            <FreelanceProject title="EasyMinecraftHosting" img="/emh.png" description="Website Design" link="https://easyminecrafthosting.com" />
+            <FreelanceProject title="Modern Hosting" img="/modern.png" description="Website Design" link="https://modernhosting.com" />
+            <FreelanceProject title="Data Wagon" img="/datawagon.png" description="Website Design" link="https://datawagon.com" />
+          </div>
+          <div class="border rounded-lg border-white/20 p-4 flex items-center justify-between">
+            <a class="text-sm text-gray-300 font-geist hover:text-white transition-all" href="https://www.behance.net/rzrusu">View all my work on Behance</a>
+            <Icon name="uil:arrow-right" class="w-4 h-4" />
+          </div>
+        </div>
+        <div class="space-y-4">
+          <h1 class="text-2xl font-bold font-instrument">other projects</h1>
+          <div class="border border-white/20 rounded-lg">
+            <Project title="Sound Bubbles" img="/logos/sb-logo.png" description="Visualize artist albums in an interactive way" :isLast="false" :links="[{ icon: 'uil:github', url: 'https://github.com/rzrusu/songbubbles' }]" />
+            <Project title="Singleplayer Roleplay UCP" img="/logos/sprp-logo.png" description="Multiplayer game user control panel" :isLast="true" :links="[{ icon: 'uil:github', url: 'https://github.com/rzrusu/SPRP-UCP' }]" />
+          </div>
+        </div>
+        <div class="flex flex-row items-center justify-between"> 
+          <p class="text-sm text-gray-400 font-instrument">This website is open source. <a href="https://github.com/rzrusu/portfolio" class="text-blue-500 hover:text-blue-600 transition-all">View the source code</a></p>
+          <p class="text-sm text-gray-400 font-instrument">© <a href="https://github.com/rzrusu" class="underline">rzrusu</a> 2025</p>
         </div>
       </div>
     </div>
---
+++ components/FreelanceProject.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div class="border border-white/20 rounded-lg">
+        <a :href="link" target="_blank">
+            <img :src="img" :alt="title" class="h-36 w-full object-cover rounded-t-lg">
+        </a>  
+        <div class="p-4">
+            <a :href="link" target="_blank" class="text-lg font-bold font-instrument text-gray-100 hover:text-white transition-all">{{ title }}</a>
+            <p class="text-sm text-gray-300 font-geist">{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: String,
+    img: String,
+    description: String,
+    link: String,
+})
+</script>
