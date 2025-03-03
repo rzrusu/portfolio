ff04c206d5c4ba1094802030df4accff1a835e70
rzrusu <rzrusu03@gmail.com>
2025-03-04 00:09:32 +0200

    feat: Add LinkedIn link and Discord username tooltip in UserBar

--- components/UserBar.vue
+++ components/UserBar.vue
@@ -12,15 +12,18 @@
         <a href="https://github.com/rzrusu" class="w-full h-16 border-r border-white/20 hover:bg-white/10 transition-all duration-300 flex items-center justify-center">
             <img src="~/assets/icons/github.svg" alt="icon" class="w-8 h-8">
         </a>
-        <a href="#" class="w-full h-16 border-r border-white/20 hover:bg-white/10 transition-all duration-300 flex items-center justify-center">
+        <a href="https://linkedin.com/in/rusu-razvan" class="w-full h-16 border-r border-white/20 hover:bg-white/10 transition-all duration-300 flex items-center justify-center">
             <img src="~/assets/icons/linkedin.svg" alt="icon" class="w-8 h-8">
         </a>
         <a href="mailto:rzrusu03@gmail.com" class="w-full h-16 border-r border-white/20 hover:bg-white/10 transition-all duration-300 flex items-center justify-center">
             <img src="~/assets/icons/email.svg" alt="icon" class="w-8 h-8">
         </a>
-        <a href="#" class="w-full h-16 hover:bg-white/10 transition-all duration-300 flex items-center justify-center">
-            <img src="~/assets/icons/discord.svg" alt="icon" class="w-8 h-8">
-        </a>
+        <div class="tooltip-container w-full h-16 hover:bg-white/10 transition-all duration-300 flex items-center justify-center">
+            <a href="#" class="relative">
+                <img src="~/assets/icons/discord.svg" alt="icon" class="w-8 h-8">
+                <span class="tooltip">rusuu</span>
+            </a>
+        </div>
     </div>
   </div>
   <div class="border rounded-lg border-white/20 p-4 flex items-center justify-between mt-4">
@@ -28,5 +31,45 @@
             <Icon name="uil:arrow-right" class="w-4 h-4" />
   </div>
 </template>
+
+<style scoped>
+.tooltip-container {
+  position: relative;
+}
+
+.tooltip {
+  visibility: hidden;
+  position: absolute;
+  bottom: -30px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+  text-align: center;
+  border-radius: 6px;
+  padding: 4px 8px;
+  font-size: 12px;
+  white-space: nowrap;
+  opacity: 0;
+  transition: opacity 0.3s;
+  z-index: 10;
+}
+
+.tooltip::after {
+  content: "";
+  position: absolute;
+  bottom: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent transparent rgba(0, 0, 0, 0.8) transparent;
+}
+
+.tooltip-container a:hover .tooltip {
+  visibility: visible;
+  opacity: 1;
+}
+</style>
 
 
